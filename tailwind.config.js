08115d30e5fae93b2b1156f9f4562e08eab90cc7
mohamed sameh <you@example.com>
2024-03-21 06:50:38 +0200

    V-1

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ export default {
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        'landing-img': "url('./background-landing.jpg')",
+      },
       fontFamily: {
         nunito: ["Nunito", 'sans-serif']
       },
@@ -19,17 +22,17 @@ export default {
           '100%': { borderRadius: '60% 50% 70% 70% / 60% 70% 70% 50%' },
         },
         lanidingAnim: {
-          '0%': { filter: 'blur(100px)' }, 
-          '10%': { filter: 'blur(90px)' }, 
-          '20%': { filter: 'blur(80px)' }, 
-          '30%': { filter: 'blur(70px)' }, 
-          '40%': { filter: 'blur(60px)' }, 
-          '50%': { filter: 'blur(50px)' }, 
-          '60%': { filter: 'blur(40px)' }, 
-          '70%': { filter: 'blur(30px)' }, 
-          '80%': { filter: 'blur(20px)' }, 
-          '90%': { filter: 'blur(10px)' }, 
-          '100%': { filter:' blur(0px)' }, 
+          '0%': { filter: 'blur(100px)' },
+          '10%': { filter: 'blur(90px)' },
+          '20%': { filter: 'blur(80px)' },
+          '30%': { filter: 'blur(70px)' },
+          '40%': { filter: 'blur(60px)' },
+          '50%': { filter: 'blur(50px)' },
+          '60%': { filter: 'blur(40px)' },
+          '70%': { filter: 'blur(30px)' },
+          '80%': { filter: 'blur(20px)' },
+          '90%': { filter: 'blur(10px)' },
+          '100%': { filter: ' blur(0px)' },
         },
       },
       animation: {
